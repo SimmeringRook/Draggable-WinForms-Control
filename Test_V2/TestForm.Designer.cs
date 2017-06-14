17bc4809fe74f32557efdabc963c6d6bafa53266
Thomas Knudson <knudsont6467@student.faytechcc.edu>
2017-06-13 17:35:00 -0700

    Add
    Completed a working example of Drag and Drop funcitonality with WinForms
    controls with comments.

---
+++ Test_V2/TestForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Test_V2
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 143);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AllowDrop = true;
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(12, 161);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Padding = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(200, 143);
+            this.flowLayoutPanel3.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AllowDrop = true;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 310);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(200, 143);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 463);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Drag Drop Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ Test_V2/TestForm.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows.Forms;
+
+namespace Test_V2
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+
+            CreateLabels(); //Generates labels to display the functionality
+            SetUpContainers();
+        }
+        
+        private void CreateLabels()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Label label = new Label();
+
+                if (i < 2)
+                {
+                    label.Text = "Draggable Label";
+                    label.MouseDown += label_MouseDownEventHandler;
+                }
+                else
+                {
+                    label.Text = "Standard Label";
+                }
+
+                label.AutoSize = true;
+                label.BorderStyle = BorderStyle.FixedSingle;
+
+                flowLayoutPanel1.Controls.Add(label);
+            }
+        }
+
+        /// <summary>
+        /// Setting up the two flow layout panels that I want to be able
+        /// to drag labels into
+        /// 
+        /// Allow the drop to occur:
+        /// containerName.AllowDrop = true; 
+        /// ----
+        /// Subscribe to Event Handlers for Drag and Drop:
+        /// containerName.DragEnter += container_DragEnterEventHandler;
+        /// containerName.DragDrop += container_DragDropEventHandler;
+        /// </summary>
+        private void SetUpContainers()
+        {
+            flowLayoutPanel3.AllowDrop = true; 
+            flowLayoutPanel3.DragEnter += container_DragEnterEventHandler;
+            flowLayoutPanel3.DragDrop += container_DragDropEventHandler;
+
+            flowLayoutPanel2.AllowDrop = true;
+            flowLayoutPanel2.DragEnter += container_DragEnterEventHandler;
+            flowLayoutPanel2.DragDrop += container_DragDropEventHandler;
+
+            //This means you can freely drag the 2 draggable labels between
+            //flow2 and flow 3 as much as you want
+            //However, since flow1 doesn't have AllowDrop set to true, you
+            //will not be able to drag the labels back into the original container
+        }
+
+        #region Draggable Control
+
+        /* Really all that has to happen from the control that you want to drag is
+         * to have them subscribe to this event handler;
+         *          "label.MouseDown += label_MouseDownEvent;"
+         */
+        private void label_MouseDownEventHandler(object sender, MouseEventArgs e)
+        {
+            DoDragDrop(sender, DragDropEffects.Move);
+        }
+
+
+        /* If you wanted to do something fancy, like.. gray out the control that is
+         * being dragged, we'd make an event handler here, and have the control also
+         * subscribe to it upon creation.
+         */
+        #endregion
+
+        #region Container Drag Drop Code
+
+        /* This event handler checks to see if the container (that has the dragged 
+         * control over it) allows Droping
+         *  if true, 
+         *      it will change the mouse cursor to inform the user they can drop
+         *  else
+         *      it will change the cursor to a "no access" cursor
+         */
+        private void container_DragEnterEventHandler(object sender, DragEventArgs e)
+        {
+            if ((e.AllowedEffect & DragDropEffects.Move) != 0)
+                e.Effect = DragDropEffects.Move;
+        }
+
+        /* This event handler is the actual behaviour for what the container (that 
+         * has the dragged control over it) needs to do when the mouse is released;
+         * thereby finishing the drop.
+         * 
+         * In this case, we want to add the control to the container.
+         */
+        private void container_DragDropEventHandler(object sender, DragEventArgs e)
+        {
+            (sender as FlowLayoutPanel).Controls.Add(e.Data.GetData(typeof(Label)) as Label);
+        }
+        #endregion
+    }
+}
